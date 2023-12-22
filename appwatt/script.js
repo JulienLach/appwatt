12cc3df5c7891db4ef7aac2681d68b6c37e63072
JulienLach <leadedchamps@gmail.com>
2023-12-22 15:11:13 +0100

    ok

--- appwatt/script.js
+++ appwatt/script.js
@@ -1,29 +1,24 @@
 // Récupération et déclaration des éléments a intégrer
-
 const wattsInput = document.getElementById('watts');
 const hoursInput = document.getElementById('hours');
+const kwhInput = document.getElementById('kwh')
 const calculButton = document.getElementById('calculer');
 const resultatElement = document.getElementById('resultat');
 const costElement = document.getElementById('cout');
 
-// Ajout de la fonction
 
+// Déclaration de la fonction
 calculButton.addEventListener('click', () => {
-
-    // Récupération des inputs
-    const watts = wattsInput.valueAsNumber;
-    const hours = hoursInput.valueAsNumber;
+    const watts = wattsInput.value;
+    const hours = hoursInput.value;
 
     //Calcul des kWh
     const kWh = (watts * hours) / 1000;
 
-
     //calcul du cout en euros
-    const coutEnEuros = kWh * 0.15;
-
+    const coutEnEuros = kWh * kwhInput.value;
 
     //Appel et affichage du résultat
-    resultatElement.textContent = `Résultat: ${kWh.toFixed(2)} kWh`;
-    costElement.textContent = `Cout en euros ${coutEnEuros.toFixed(2)} €`;
-
+    resultatElement.textContent = "Resultat : " + kWh.toFixed(2) + " kWh"
+    costElement.textContent = "Cout en euros : " + coutEnEuros.toFixed(2) + "€";
 });

314c13f987c059676a3a49e694f8fe129e107a00
Julien Lach <leadedchamps@gmail.com>
2024-01-31 22:29:51 +0100

    ok

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 // Récupération et déclaration des éléments a intégrer
-const wattsInput = document.getElementById('watts');
-const hoursInput = document.getElementById('hours');
-const kwhInput = document.getElementById('kwh')
-const calculButton = document.getElementById('calculer');
-const resultatElement = document.getElementById('resultat');
-const costElement = document.getElementById('cout');
-
+const wattsInput = document.getElementById("watts");
+const hoursInput = document.getElementById("hours");
+const kwhInput = document.getElementById("kwh");
+const calculButton = document.getElementById("calculer");
+const resultatElement = document.getElementById("resultat");
+const costElement = document.getElementById("cout");
 
 // Déclaration de la fonction
-calculButton.addEventListener('click', () => {
-    const watts = wattsInput.value;
-    const hours = hoursInput.value;
+calculButton.addEventListener("click", () => {
+  const watts = wattsInput.value;
+  const hours = hoursInput.value;
 
-    //Calcul des kWh
-    const kWh = (watts * hours) / 1000;
+  //Calcul des kWh
+  const kWh = (watts * hours) / 1000;
 
-    //calcul du cout en euros
-    const coutEnEuros = kWh * kwhInput.value;
+  //calcul du cout en euros
+  const coutEnEuros = kWh * kwhInput.value;
 
-
-    //Appel et affichage du résultat
-    resultatElement.textContent = "Resultat : " + kWh.toFixed(2) + " kWh"
-    costElement.textContent = "Cout en euros : " + coutEnEuros.toFixed(2) + "€";
+  //Appel et affichage du résultat
+  resultatElement.textContent = "Resultat : " + kWh.toFixed(2) + " kWh";
+  costElement.textContent = "Cout en euros : " + coutEnEuros.toFixed(2) + "€";
 });
